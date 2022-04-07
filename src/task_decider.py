59d32bfb251e31f3162be842896eedb8009dabee
Fiona <fiona_berkery@hotmail.com>
2022-04-07 15:05:15 +0100

    advanced tests for 5 tasks completed

--- src/task_decider.py
+++ src/task_decider.py
@@ -1,7 +1,9 @@
 def get_preferred_option(task1, task2):
-    if task1.preferred_over == task2.description:
+    if task2.description in task1.preferred_over:
         return task1.description
     else:
         return task2.description
         
+
+
 
--- tests/task_decider_tests.py
+++ tests/task_decider_tests.py
@@ -6,14 +6,16 @@ from src.task import Task
 class TestTaskDecider(unittest.TestCase):
     
     def setUp(self):
-        self.wash_dishes = Task("wash dishes", 30, "cook dinner")
-        self.cook_dinner = Task("cook dinner", 45, "clean windows")
-        self.clean_windows = Task("clean windows", 60, "wash dishes")
+        self.wash_dishes = Task("wash dishes", 30, ["cook dinner", "wash clothes"])
+        self.cook_dinner = Task("cook dinner", 45, ["clean windows", "do ironing"])
+        self.do_ironing = Task("do_ironing", 60, ["wash dishes", "wash clothes"])
+        self.wash_clothes = Task("wash_clothes", 60, ["cook dinner", "clean windows"])
+        self.clean_windows = Task("clean windows", 60, ["do ironing", "wash dishes"])
 
-    # def test_wash_dishes_preferred_over_cook_dinner(self):
+    def test_wash_dishes_preferred_over_cook_dinner(self):
         self.assertEqual("wash dishes", get_preferred_option(self.wash_dishes, self.cook_dinner))
         self.assertEqual("wash dishes", get_preferred_option(self.cook_dinner, self.wash_dishes))
 
-    # def test_cook_dinner_preferred_over_clean_windows(self):
+    def test_cook_dinner_preferred_over_clean_windows(self):
         self.assertEqual("cook dinner", get_preferred_option(self.cook_dinner, self.clean_windows))
         self.assertEqual("cook dinner", get_preferred_option(self.clean_windows, self.cook_dinner))
